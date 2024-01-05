7a985b60f5d37010ba3336407af0dcd8cd5b47b2
Kopacz Gabriel <kopaczgabi@gmail.com>
2024-01-05 22:22:09 +0200

    Initial setup of theme, dependencies, basic setup.

---
+++ lib/constants/appwrite_constants.dart
@@ -0,0 +1,5 @@
+class AppwriteConstants {
+  static const String databaseId = '65985153b63597139eb0';
+  static const String projectId = '65982f468fe9cf6cb109';
+  static const String endPoint = 'http://localhost:80/v1';
+}
